1cbf9debf9cc7eff733c55114ce0b0a0ee9f81aa
YauheniHarbuzau <rimestorm7@gmail.com>
2023-02-10 02:38:38 +0300

    add buildSrc module

---
+++ buildSrc/src/main/groovy/ru/clevertec/plugin/CustomPlugin.groovy
@@ -0,0 +1,12 @@
+package ru.clevertec.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class CustomPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        println "Clevertec Task 02"
+    }
+}
